f24f55a553539e3f5db53d24070f79c8f41cebeb
Ajay Joshi <joshiajay311@gmail.com>
2024-04-21 18:53:21 +0530

    new user connected broadcast

--- public/script.js
+++ public/script.js
@@ -1,9 +1,17 @@
+const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
-
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
 let myVideoStream; // created a variable outside so that to access it throughtout
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
 
 navigator.mediaDevices // this promise is to capture video and audio from the device
   .getUserMedia({
@@ -15,10 +23,14 @@ navigator.mediaDevices // this promise is to capture video and audio from the de
     addVideoStream(myVideo, stream);
   });
 
-const addVideoStream = (video, stream) => {
-  video.srcObject = stream;
-  video.addEventListener("loadmetadata", () => {
-    video.play();
-  });
-  videoGrid.append(video);
-};
+socket.emit("join-room", ROOM_ID);
+socket.on('user-connected', () => {
+   connectToNewUSer();
+})
+
+const connectToNewUSer = () => {
+   console.log("new user connected")
+}
+
+
+
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
 
 app.set("view engine", "ejs"); //this is for rendering the web page other view engines are Handlebars, moustache
 app.use(express.static("public")); //middleware to serve static files
-
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`); // redirects the landing page to a URL containing RoomID created through uuid
@@ -15,4 +15,11 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomID: req.params.room });
 });
 
-server.listen(3030);
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomID) => {
+    socket.join(roomID);
+    socket.broadcast.emit('user-connected');
+  });
+});
+
+server.listen(3000);
