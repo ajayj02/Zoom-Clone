1b75ffaf6aa44ed34ff37e8ee3f6d320c13d0734
Ajay Joshi <joshiajay311@gmail.com>
2024-04-21 19:26:19 +0530

    initial code to add P2P connection feature

--- public/script.js
+++ public/script.js
@@ -3,6 +3,12 @@ const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
+var peer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '3000'
+})
+
 let myVideoStream; // created a variable outside so that to access it throughtout
 
 const addVideoStream = (video, stream) => {
@@ -23,6 +29,9 @@ navigator.mediaDevices // this promise is to capture video and audio from the de
     addVideoStream(myVideo, stream);
   });
 
+peer.on('open', id => {
+  console.log(id)
+})
 socket.emit("join-room", ROOM_ID);
 socket.on('user-connected', () => {
    connectToNewUSer();
--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
 app.set("view engine", "ejs"); //this is for rendering the web page other view engines are Handlebars, moustache
 app.use(express.static("public")); //middleware to serve static files
 
+app.use("/peerjs", peerServer);
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`); // redirects the landing page to a URL containing RoomID created through uuid
 });
@@ -18,7 +23,7 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
   socket.on("join-room", (roomID) => {
     socket.join(roomID);
-    socket.broadcast.emit('user-connected');
+    socket.broadcast.emit("user-connected");
   });
 });
 
